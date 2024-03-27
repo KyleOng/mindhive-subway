edc6176694e86e214f6c15b196f154805f95ce71
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2024-03-27 23:27:42 +0800

    feat: add create_outlet post request

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
-from .db import SessionLocal, engine
+from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
+
 from . import models, schemas
+from .db import SessionLocal
 
 
 def get_db():
@@ -13,7 +14,6 @@ def get_db():
 
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
@@ -24,3 +24,18 @@ async def root():
 @app.get("/outlets", response_model=list[schemas.Outlet])
 async def read_outlets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return db.query(models.Outlet).offset(skip).limit(limit).all()
+
+
+@app.post("/outlet", response_model=schemas.Outlet)
+async def create_outlet(outlet: schemas.OutletCreate, db: Session = Depends(get_db)):
+    db_outlet = models.Outlet(
+        name=outlet.name,
+        address=outlet.address,
+        waze_link=outlet.waze_link,
+        longitude=outlet.longitude,
+        latitude=outlet.latitude,
+    )
+    db.add(db_outlet)
+    db.commit()
+    db.refresh(db_outlet)
+    return db_outlet
