1770e7ff506fa2a3d5a2c409165e7eb89d848806
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2024-03-30 22:32:47 +0800

    fix: update prompts

--- server/lib/prompts.py
+++ server/lib/prompts.py
@@ -20,37 +20,37 @@ where address like '%Sri Petaling%';
         """,
     },
     {
-        "input": "Which outlet closed the earliest?",
-        "query": """
-select o.name, o.longitude, o.latitude, min_end_time as earlier_close_time
-from outlets o join (
-    select outlet_id, min(end_time) as min_end_time
-    from operating_hours oh
-    group by outlet_id
-) h on
-o.id = h.outlet_id
-order by min_end_time
-limit {top_k}+1;
-        """,
-    },
-    {
         "input": "Which outlet closed the latest?",
         "query": """
-select o.name, o.longitude, o.latitude, max_end_time as latest_close_time
+with r as (
+select o.name, max_end_time as latest_close_time
 from outlets o join (
     select outlet_id, max(end_time) as max_end_time
     from operating_hours oh
     group by outlet_id
-) h on
-o.id = h.outlet_id
-order by max_end_time desc
-limit {top_k}+1;
-        """,
+) h on o.id = h.outlet_id)
+select name, latest_close_time from r
+where r.latest_close_time = (select max(latest_close_time) from r)""",
+    },
+    {
+        "input": "Which outlets open the latest on monday?",
+        "query": """
+with r as (
+select o.name, max_start_time as latest_open_time
+from outlets o join (
+    select outlet_id, max(start_time) as max_start_time
+    from operating_hours oh
+    where day_of_week = 1
+    group by outlet_id
+) h on o.id = h.outlet_id)
+select name, latest_open_time from r
+where r.latest_open_time = (select max(latest_open_time) from r)
+""",
     },
     {
         "input": "Which outlets closed on sunday?",
         "query": """
-select name, o.longitude, o.latitude
+select o.id, o.name
 from outlets o left join (
     select outlet_id
     from operating_hours oh
@@ -64,8 +64,8 @@ limit {top_k}+1;
     {
         "input": "Can you provide the outlets that is available at 6:30am?",
         "query": """
-select name, longitude, latitude
-from outlets
+select o.id, o.name, 
+from outlets o
 where id in (
 	select distinct outlet_id
 	from operating_hours oh
@@ -85,22 +85,9 @@ limit {top_k}+1;
         """,
     },
     {
-        "input": "Which outlets open the latest on monday?",
-        "query": """
-select o.name, o.longitude, o.latitude, max_start_time as latest_open_time
-from outlets o join (
-    select outlet_id, max(start_time) as max_start_time
-    from operating_hours oh
-    where day_of_week = 1
-    group by outlet_id
-) h on o.id = h.outlet_id
-order by max_start_time desc
-limit {top_k}+1;""",
-    },
-    {
         "input": "Which outlet open currently?",
         "query": """
-select o.name, o.longitude, o.latitude, start_time
+select o.id, o.name, start_time
 from outlets o
 join operating_hours oh on
 o.id = oh.outlet_id
@@ -126,7 +113,7 @@ Only use the given tools. Only use the information returned by the tools to cons
 You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-Avoid querying the outlet address unless the user explicitly requests it. When displaying outlet results, ensure that only the name, longitude, and latitude are returned in the format of "name" ("longitude" "latitude"). Longitude and Latitude are always decimal.
+Avoid querying the outlet address unless the user explicitly requests it.
 
 If the user query is plural and without aggregate function like count(*), then limit {top_k} + 1. If the results is {top_k} + 1 rows, the response should followed by "etc" at the end, else return {top_k} or less. Also, remember to add input context prefix. For example, if the input is "What outlets are open at 10am?", the response prefix should be "The outlets that are open at 10am," followed by a line break and the SQL result.
 
@@ -134,7 +121,7 @@ If the user query is singular, then try to use limit 1 in the sql query. For exa
 
 If the result is empty, the response should depend on the user's query. For instance, "Sorry, there are no outlets open at ..." or "Sorry, couldn't find any outlets open on Friday ..."
 
-"If the user requests a query related to operating hours, return the outlet along with its operating hours based on the context. For example, "name" ("longitude" "latitude") opens at "start_time" on "day_of_week".
+"If the user requests a query related to operating hours, return the outlet along with its operating hours based on the context. For example, "name" (outlet_id "id") opens at "start_time" on "day_of_week".
 
 If the query is not related to the database or seems malicious, or if it attempts to exhaust the database's resources, simply return "I don't know if I can do that" as the answer.
 Here are some examples of user inputs and their corresponding SQL queries:
