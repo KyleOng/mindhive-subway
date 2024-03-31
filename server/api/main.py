6e8ab70943c4b7cf800be6352a5ab0d081ba73c1
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2024-04-01 04:07:10 +0800

    feat: change app to api

---
+++ server/api/main.py
@@ -0,0 +1,92 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from datetime import datetime, timezone
+
+import uvicorn
+from api import models, schemas
+from api.db import SessionLocal
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from lib.geocoding import get_distance_between
+from lib.llm import agent_executor
+from sqlalchemy.orm import Session
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app = FastAPI()
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/outlets", response_model=list[schemas.Outlet])
+async def read_outlets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return db.query(models.Outlet).offset(skip).limit(limit).all()
+
+
+@app.post("/outlets/within_radius")
+async def get_outlets_within_radius(
+    latitude: float, longitude: float, distance: int, db: Session = Depends(get_db)
+):
+    outlets = db.query(models.Outlet).all()
+    outlets_within_radius = []
+    for outlet in outlets:
+        distance_between = get_distance_between(
+            (outlet.latitude, outlet.longitude), (latitude, longitude)
+        )
+        if distance_between > 0 and distance_between <= distance:
+            outlets_within_radius.append(
+                {
+                    **outlet.__dict__,
+                    "distance": distance_between,
+                }  # hacky way to turn schema to dict using __dict__
+            )
+    return outlets_within_radius
+
+
+@app.post("/ask")
+async def ask(input: str):
+    try:
+        result = agent_executor.invoke(
+            {
+                "input": input,
+                "dialect": "PostgreSQL",
+                "top_k": 10,
+            }
+        )
+        return {
+            "input": input,
+            "output": result["output"],
+            "error": False,
+            "datetime": datetime.now(timezone.utc).isoformat(),
+        }
+    except Exception as e:
+        return {
+            "input": input,
+            "output": "Internal Server Error. Please contact the admin to resolve this issue.",
+            "error": True,
+            "datetime": datetime.now(timezone.utc).isoformat(),
+        }
+
+if __name__ == "__main__":
+  uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ server/api/schemas.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from datetime import datetime, time
+
+
+class OutletBase(BaseModel):
+    name: str
+    address: str
+    waze_link: str
+    latitude: float = 0
+    longitude: float = 0
+
+
+class OutletCreate(OutletBase):
+    pass
+
+
+class Outlet(OutletBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class OperatingHourBase(BaseModel):
+    day_of_week: str
+    start_time: time
+    end_time: time
+
+
+class OperatingHourCreate(OperatingHourBase):
+    pass
+
+
+class OperatingHour(OperatingHourBase):
+    outlets: list[Outlet] = []
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
