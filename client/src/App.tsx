27eb7d58aba965b029907172e47996610c02decd
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2024-04-01 17:53:54 +0800

    feat: add every components to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import {
-  APIProvider,
-  AdvancedMarker,
-  InfoWindow,
-  Map,
-  Pin,
-} from "@vis.gl/react-google-maps";
-import { useEffect, useState } from "react";
-import QueryResults from "./components/query-results";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useEffect, useRef, useState } from "react";
+import Marker from "./components/marker";
+import QnAs from "./components/qnas";
 import { Outlet } from "./interfaces/outlet.interface";
+import { useHighlightedMarkersStore } from "./store/highlighted-markers";
+import { useInfoWindowStore } from "./store/info-window";
+import Select from "./components/select";
 
 const API_URL = import.meta.env.VITE_API_URL;
 const GOOGLE_GEOCODING_API_KEY = import.meta.env.VITE_GOOGLE_GEOCODING_API_KEY;
 const GOOGLE_MAP_ID = import.meta.env.VITE_GOOGLE_MAP_ID;
+const KUALA_LUMPUR_POSITION = { lat: 3.1319, lng: 101.6841 };
 
 const App = () => {
-  const position = { lat: 3.1319, lng: 101.6841 };
   const [outlets, setOutlets] = useState<Outlet[]>([]);
-
-  const [open, setOpen] = useState(false);
-  const [outlet, setOutlet] = useState<Outlet>();
+  const { setInfoWindow } = useInfoWindowStore();
+  const { setHighlightedMarkers } = useHighlightedMarkersStore();
+  const selectRef = useRef<HTMLSelectElement>(null);
 
   useEffect(() => {
     fetchData();
@@ -41,40 +39,30 @@ const App = () => {
       <APIProvider apiKey={GOOGLE_GEOCODING_API_KEY}>
         <Map
           style={{ width: "100vw", height: "100vh" }}
-          defaultCenter={position}
-          defaultZoom={14}
+          defaultCenter={KUALA_LUMPUR_POSITION}
+          defaultZoom={13}
           disableDefaultUI={true}
           mapId={GOOGLE_MAP_ID}
-          onClick={() => {
-            setOpen(false);
-          }}
           disableDoubleClickZoom
+          onClick={() => {
+            setHighlightedMarkers([]);
+            setInfoWindow({ id: "", open: false });
+            if (selectRef.current) {
+              selectRef.current.selectedIndex = 0;
+            }
+          }}
         >
           {outlets.map((outlet, key) => (
-            <AdvancedMarker
-              key={key}
-              position={{ lat: outlet.latitude, lng: outlet.longitude }}
-              onClick={() => {
-                setOpen(true);
-                setOutlet(outlet);
-              }}
-            >
-              <Pin />
-            </AdvancedMarker>
+            <Marker key={key} outlet={outlet} />
           ))}
-          {open && outlet !== undefined && (
-            <InfoWindow
-              position={{ lat: outlet.latitude, lng: outlet.longitude }}
-              onCloseClick={() => setOpen(false)}
-            >
-              <p>{outlet.name}</p>
-              <p>{outlet.address}</p>
-            </InfoWindow>
-          )}
         </Map>
       </APIProvider>
       <div className="fixed right-0 top-0 m-5">
-        <QueryResults />
+        <QnAs />
+      </div>
+
+      <div className="fixed left-0 top-0 m-5">
+        <Select selectRef={selectRef} outlets={outlets}/>
       </div>
     </div>
   );
